8ba9cea22f042faba95c46afdaba79ec9d471ca1
Dobinda Nicolae <166544044+JuicyDawg@users.noreply.github.com>
2026-03-09 14:21:27 +0200

    Update server.js

--- ITTI-Smart-Town-App/Backend/server.js
+++ ITTI-Smart-Town-App/Backend/server.js
@@ -1,11 +1,12 @@
 console.log("SERVER FILE STARTED");
 
-
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 const places = [
